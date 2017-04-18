47008e4ee39b5407d61cce6defa2bbb53d91f9e0
Josh <c13joshua.christman@gmail.com>
2017-04-18 00:19:03 -0400

    - Theoretically, time of day is now support.

--- src/motion-accessory.js
+++ src/motion-accessory.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 import {version} from '../package.json';
 import wait from './wait.js';
 
@@ -11,6 +13,23 @@ class MotionSensor {
         ({ Accessory, Characteristic, Service } = HOMEBRIDGE);
         this.log = log;
         this.config = timer_config;
+
+        if (this.config.active !== undefined) {
+            this.active = this.config.active;
+        } else {
+            this.active = {
+                start: '00:00',
+                end: '24:00'
+            };
+        }
+
+        this.active = {
+            start: moment(this.active.start, 'HH:mm'),
+            end: moment(this.active.end, 'HH:mm')
+        }
+
+        this.active_cross_days = this.active.end.isBefore(this.active.start);
+
         this.is_start = is_start_or_end_sensor === MotionSensor.START;
         this.name = `${this.config.name} ${this.is_start ? 'Start' : 'End'}`;
     }
@@ -45,6 +64,7 @@ class MotionSensor {
 
     async activate() {
         this.log(`Activated ${this.name}`);
+        check_time();
         this.detectedChar.setValue(true);
         await wait(5000); // Wait 10 seconds
         this.deactivate();
@@ -53,6 +73,28 @@ class MotionSensor {
     deactivate() {
         this.log(`Deactivated ${this.name}`);
         this.detectedChar.setValue(false);
+    }
+
+    check_time() {
+        const cur = moment();
+        const hour = cur.hour();
+        const minute = cur.minute();
+        
+        if (this.active_cross_days) {
+            if (this.active.start.hour() <= hour || hour <= this.active.end.hour()) {
+                if (this.active.start.minute() <= minute || minute <= this.active.end.minute()) {
+                    return true;
+                }
+            }
+        } else {
+            if (this.active.start.hour() <= hour && hour <= this.active.end.hour()) {
+                if (this.active.start.minute() <= minute && minute <= this.active.end.minute()) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
 
